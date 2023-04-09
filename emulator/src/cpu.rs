23e1fd988c9972cc90f5c38e928d3900f2c1730a
gabizon103 <ehg54@cornell.edu>
2023-04-09 10:00:21 -0400

    add a bunch more instrs

--- emulator/src/cpu.rs
+++ emulator/src/cpu.rs
@@ -1,8 +1,26 @@
+use std::collections::HashMap;
+use crate::opcodes;
+
+
+bitflags! {
+    pub struct CPUFlags:u8 {
+        const CARRY             = 0b0000_0001;
+        const ZERO              = 0b0000_0010;
+        const INTERRUPT_DISABLE = 0b0000_0100;
+        const DECIMAL_MODE      = 0b0000_1000;
+        const BREAK             = 0b0001_0000;
+        const BREAK2            = 0b0010_0000;
+        const OVERFLOW          = 0b0100_0000;
+        const NEGATIVE          = 0b1000_0000;
+    }
+}
+
+
 pub struct CPU {
     pub reg_a:u8,
     pub reg_x:u8,
     pub reg_y:u8,
-    pub status:u8,
+    pub status:CPUFlags,
     pub pc:u16,
     memory:[u8;0xffff]
 }
@@ -28,7 +46,7 @@ impl CPU {
             reg_a: 0,
             reg_x: 0,
             reg_y: 0,
-            status: 0,
+            status: CPUFlags::from_bits_truncate(0b100100),
             pc: 0,
             memory:[0;0xffff]
         }
@@ -82,7 +100,7 @@ impl CPU {
         }
     }
 
-    fn mem_read(&mut self,addr:u16) -> u8 {
+    fn mem_read(&self,addr:u16) -> u8 {
         self.memory[addr as usize]
     }
 
@@ -90,7 +108,7 @@ impl CPU {
         self.memory[addr as usize] = data;
     }
 
-    fn mem_read_u16(&mut self,pos:u16) -> u16 {
+    fn mem_read_u16(&self,pos:u16) -> u16 {
         let lo = self.mem_read(pos) as u16;
         let hi = self.mem_read(pos+1) as u16;
         (hi << 8) | (lo as u16)
@@ -107,7 +125,7 @@ impl CPU {
         self.reg_a = 0;
         self.reg_x = 0;
         self.reg_y = 0;
-        self.status = 0;
+        self.status = CPUFlags::from_bits_truncate(0b100100);
         self.pc = self.mem_read_u16(0xfffc);
     }
 
@@ -124,25 +142,70 @@ impl CPU {
 
     fn update_z_and_n(&mut self,result:u8) {
         if result == 0 {
-            self.status = self.status | 0b0000_0010; // set Z flag 1
+            self.status.insert(CPUFlags::ZERO); // set Z flag 1
         }
         else {
-            self.status = self.status & 0b1111_1101; // set Z flag 0
+            self.status.remove(CPUFlags::ZERO); // set Z flag 0
         }
         if result & 0b1000_0000 != 0 {
-            self.status = self.status | 0b1000_0000; // set N flag 1
+            self.status.insert(CPUFlags::NEGATIVE); // set N flag 1
         }
         else {
-            self.status = self.status & 0b0111_1111;
+            self.status.remove(CPUFlags::NEGATIVE);
         }
+    }
+
+    fn set_carry_flag(&mut self) {
+        self.status.insert(CPUFlags::CARRY);
+    }
+
+    fn clear_carry_flag(&mut self) {
+        self.status.remove(CPUFlags::CARRY);
+    }
+
+    fn set_decimal_flag(&mut self) {
+        self.status.insert(CPUFlags::DECIMAL_MODE);
+    }
+
+    fn clear_decimal_flag(&mut self) {
+        self.status.remove(CPUFlags::DECIMAL_MODE);
+    }
+
+    fn set_overflow_flag(&mut self) {
+        self.status.insert(CPUFlags::OVERFLOW);
+    }
+
+    fn clear_overflow_flag(&mut self) {
+        self.status.remove(CPUFlags::OVERFLOW);
+    }
+
+    fn set_interrupt_flag(&mut self) {
+        self.status.insert(CPUFlags::INTERRUPT_DISABLE);
+    }
+
+    fn clear_interrupt_flag(&mut self) {
+        self.status.remove(CPUFlags::INTERRUPT_DISABLE);
+    }
+
+    fn set_reg_a(&mut self, value:u8) {
+        self.reg_a = value;
+        self.update_z_and_n(self.reg_a);
+    }
+
+    fn set_reg_x(&mut self, value:u8) {
+        self.reg_x = value;
+        self.update_z_and_n(self.reg_x);
+    }
+
+    fn set_reg_y(&mut self, value:u8) {
+        self.reg_y = value;
+        self.update_z_and_n(self.reg_y);
     }
 
     fn lda(&mut self,mode:&AddressingMode) {
         let addr = self.get_operand_address(mode);
         let value = self.mem_read(addr);
-
-        self.reg_a = value;
-        self.update_z_and_n(self.reg_a);
+        self.set_reg_a(value);
     }
 
     fn sta(&mut self,mode:&AddressingMode) {
@@ -151,13 +214,177 @@ impl CPU {
     }
 
     fn tax(&mut self) { // transfer A to X
-        self.reg_x = self.reg_a;
-        self.update_z_and_n(self.reg_x);
+        self.set_reg_x(self.reg_a);
     }
 
     fn inx(&mut self) { 
-        self.reg_x = self.reg_x.wrapping_add(1);
-        self.update_z_and_n(self.reg_x);
+        self.set_reg_x(self.reg_x.wrapping_add(1));
+    }
+
+    fn and(&mut self,mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.set_reg_a(self.reg_a & value);
+    }
+
+    fn asl(&mut self,mode:&AddressingMode) -> u8 {
+        let addr = self.get_operand_address(mode);
+        let mut value = self.mem_read(addr);
+        if value >> 7 == 1 {
+            self.set_carry_flag();
+        } else {
+            self.clear_carry_flag();
+        }
+        value = value << 1;
+        self.mem_write(addr,value);
+        self.update_z_and_n(value);
+        value
+    }
+
+    fn asl_acc(&mut self) {
+        let mut value = self.reg_a;
+        if value >> 7 == 1 {
+            self.set_carry_flag();
+        } else {
+            self.clear_carry_flag();
+        }
+        value = value << 1;
+        self.reg_a = value;
+    }
+
+    fn branch(&mut self,condition:bool) {
+        if condition {
+            let jump:i8 = self.mem_read(self.pc) as i8;
+            let jump_addr = self.pc.wrapping_add(1).wrapping_add(jump as u16);
+            self.pc = jump_addr;
+        }
+    }
+
+    fn bit(&mut self, mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        let and = self.reg_a & value;
+
+        if and == 0 {
+            self.status.insert(CPUFlags::ZERO);
+        } else {
+            self.status.remove(CPUFlags::ZERO);
+        }
+    }
+
+    fn clc(&mut self) {
+        self.clear_carry_flag();
+    }
+
+    fn cld(&mut self) {
+        self.clear_decimal_flag();
+    }
+
+    fn cli(&mut self) {
+        self.clear_interrupt_flag();
+    }
+
+    fn clv(&mut self) {
+        self.clear_overflow_flag();
+    }
+
+    fn cmp(&mut self, mode:&AddressingMode, cmp_with:u8) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+
+        if value <= cmp_with {
+            self.set_carry_flag();
+        } else {
+            self.clear_carry_flag();
+        }
+        self.update_z_and_n(cmp_with.wrapping_sub(value));
+    }
+
+    fn dec(&mut self, mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        let new = value.wrapping_sub(1);
+
+        self.mem_write(addr,new);
+        self.update_z_and_n(new);
+    }
+
+    fn dex(&mut self) {
+        let new = self.reg_x.wrapping_sub(1);
+        self.set_reg_x(new);
+    }
+
+    fn dey(&mut self) {
+        let new = self.reg_y.wrapping_sub(1);
+        self.set_reg_y(new);
+    }
+
+    fn eor(&mut self, mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let data = self.mem_read(addr);
+        self.set_reg_a(self.reg_a ^ data);
+    }
+
+    fn inc(&mut self, mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let mut data = self.mem_read(addr);
+        data = data.wrapping_add(1);
+        self.mem_write(addr,data);
+        self.update_z_and_n(data);
+    }
+
+    fn inx(&mut self) {
+        self.set_reg_x(self.reg_x.wrapping_add(1));
+    }
+
+    fn iny(&mut self) {
+        self.set_reg_y(self.reg_y.wrapping_add(1));
+    }
+
+    fn ldx(&mut self, mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let data = self.mem_read(addr);
+        self.set_reg_x(data);
+    }
+
+    fn ldy(&mut self, mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let data = self.mem_read(addr);
+        self.set_reg_y(data);
+    }
+
+    fn lsr_acc(&mut self) {
+        let mut data = self.reg_a;
+        if data & 0b0000_0001 == 1 {
+            self.set_carry_flag();
+        } else {
+            self.clear_carry_flag();
+        }
+        data = data >> 1;
+        self.set_reg_a(data);
+    }
+
+    fn lsr(&mut self, mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let mut data = self.mem_read(addr);
+        if data & 0b0000_0001 == 1 {
+            self.set_carry_flag();
+        } else {
+            self.clear_carry_flag();
+        }
+        data = data >> 1;
+        self.set_reg_a(data);
+    }
+
+    fn nop(&mut self) {
+        return
+    }
+
+    fn ora(&mut self,mode:&AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let data = self.mem_read(addr);
+        self.set_reg_a(self.reg_a | data);
+        self.update_z_and_n(self.reg_a);
     }
 
 
@@ -169,24 +396,109 @@ impl CPU {
             self.pc += 1;
             let pc_state = self.pc;
             let opcode = opcodes.get(&code).expect(&format!("OpCode {:x} is not recognized",code));
-            match opscode {
-                0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1
-                => { // LDA
-                    self.lda(&opcode.mode);
-                }
-                0x85 | 0x95 | 0x8d | 0x9d | 0x99 | 0x81 | 0x91
-                => { // STA
-                    self.sta(&opcode.mode);
-                }
-                0x00 => {
-                    return;
-                }
-                0xaa => { // TAX (transfer A to X)
-                    self.tax();
-                }
-                0xe8 => { // INX (increment X)
-                    self.inx();
-                }
+            match code {
+                // LDA
+                0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1 => {self.lda(&opcode.mode);}
+
+                // AND
+                0x29 | 0x25 | 0x35 | 0x2d | 0x3d | 0x39 | 0x21 | 0x31 => {self.and(&opcode.mode);}
+                
+                // ASL
+                0x0a => {self.asl_acc();} 
+                
+                // ASL
+                0x06 | 0x16 | 0x0e | 0x1e => {self.asl(&opcode.mode);}
+                
+                // STA
+                0x85 | 0x95 | 0x8d | 0x9d | 0x99 | 0x81 | 0x91 => {self.sta(&opcode.mode);}
+                
+                // BRK
+                0x00 => {return;}
+
+                // TAX
+                0xaa => {self.tax();}
+
+                // INX
+                0xe8 => {self.inx();}
+
+                // BIT
+                0x24 | 0x2c => {self.bit(&opcode.mode);}
+
+                // CLC
+                0x18 => { self.clc(); }
+
+                // CLD
+                0xd8 => { self.cld(); } 
+
+                // CLI
+                0x58 => { self.cli(); }
+
+                // CLV
+                0xb8 => { self.clv(); }
+
+                // CMP
+                0xc9 | 0xc5 | 0xd5 | 0xcd | 0xdd | 0xd9 | 0xc1 | 0xd1 
+                    => { self.compare(&opcode.mode,self.reg_a); }
+
+                // CPX
+                0xe0 | 0xe4 | 0xec => { self.compare(&opcode.mode, self.reg_x); }
+
+                // CPY
+                0xc0 | 0xc4 | 0xcc => { self.compare(&opcode.mode, self.reg_y); }
+
+                // DEC
+                0xc6 | 0xd6 | 0xce | 0xde => { self.dec(&opcode.mode); }
+
+                // DEX
+                0xca => { self.dex(); }
+
+                // DEY
+                0xca => { self.dey(); }
+
+                // EOR
+                0x49 | 0x45 | 0x55 | 0x4d | 0x5d | 0x59 | 0x41 | 0x51 
+                    => { self.eor(&opcode.mode); }
+
+                // INC
+                0xe6 | 0xf6 | 0xee | 0xfe => { self.inc(&opcode.mode); }
+
+                // INX 
+                0xe8 => { self.inx(); }
+
+                // INY
+                0xc8 => { self.iny(); }
+
+                // JMP (indirect addressing)
+                0x6c => { /* TODO */}
+
+                // JMP (absolute addressing)
+                0x4c => { /* TODO */}
+
+                // JSR 
+                0x20 => {/* TODO */}
+
+                // LDX
+                0xa2 | 0xa6 | 0xb6 | 0xae | 0xbe
+                    => { self.ldx(&opcode.mode); }
+
+                // LDY
+                0xa0 | 0xa4 | 0xb4 | 0xac | 0xbc
+                    => { self.ldy(&opcode.mode); }
+
+                // LSR (no addressing)
+                0x4a => { self.lsr_acc(); }
+
+                // LSR
+                0x46 | 0x56 | 0x4e | 0x5e
+                    => { self.lsr(&opcode.mode); }
+
+                // NOP
+                0xea => { self.nop(); }
+                
+                // ORA
+                0x09 | 0x05 | 0x15 | 0x0d  | 0x19 | 0x01 | 0x11
+                    => {self.ora(&opcode.mode);}
+
                 _ => { todo!(); }
             }
         }
--- emulator/src/opcodes.rs
+++ emulator/src/opcodes.rs
@@ -23,7 +23,10 @@ impl OpCode {
 
 lazy_static! {
     pub static ref CPU_OP_CODES: Vec<OpCode> = vec![
+        // BRK
         OpCode::new(0x00,"BRK",1,7,AddressingMode::NoneAddressing),
+
+        // TAX (transfer A to X)
         OpCode::new(0xaa,"TAX",1,2,AddressingMode::NoneAddressing),
 
         // LDA
@@ -36,14 +39,243 @@ lazy_static! {
         OpCode::new(0xa1,"LDA",2,6,AddressingMode::Indirect_X),
         OpCode::new(0xb1,"LDA",2,5,AddressingMode::Indirect_Y),
 
-        //STA
+        // STA
         OpCode::new(0x85,"STA",2,3,AddressingMode::ZeroPage),
         OpCode::new(0x95,"STA",2,4,AddressingMode::ZeroPage_X),
         OpCode::new(0x8d,"STA",3,4,AddressingMode::Absolute),
         OpCode::new(0x9d,"STA",3,5,AddressingMode::Absolute_X),
         OpCode::new(0x99,"STA",3,5,AddressingMode::Absolute_Y),
         OpCode::new(0x81,"STA",2,6,AddressingMode::Indirect_X),
-        OpCode::new(0x91,"STA",2,6,AddressingMode::Indirect_Y)
+        OpCode::new(0x91,"STA",2,6,AddressingMode::Indirect_Y),
+
+        // AND 
+        OpCode::new(0x29,"AND",2,2,AddressingMode::Immediate),
+        OpCode::new(0x25,"AND",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0x35,"AND",2,4,AddressingMode::ZeroPage_X),
+        OpCode::new(0x2d,"AND",3,4,AddressingMode::Absolute),
+        OpCode::new(0x3d,"AND",3,4,AddressingMode::Absolute_X),
+        OpCode::new(0x39,"AND",3,4,AddressingMode::Absolute_Y),
+        OpCode::new(0x21,"AND",2,6,AddressingMode::Indirect_X),
+        OpCode::new(0x31,"AND",2,5,AddressingMode::Indirect_Y),
+
+        // ASL (arithmetic left shift)
+        OpCode::new(0x0a,"ASL",1,2,AddressingMode::NoneAddressing),
+        OpCode::new(0x06,"ASL",2,5,AddressingMode::ZeroPage),
+        OpCode::new(0x16,"ASL",2,6,AddressingMode::ZeroPage_X),
+        OpCode::new(0x0e,"ASL",3,6,AddressingMode::Absolute),
+        OpCode::new(0x1e,"ASL",3,7,AddressingMode::Absolute_X),
+
+        // BCC (branch if carry clear)
+        OpCode::new(0x90,"BCC",2,2,AddressingMode::NoneAddressing),
+
+        // BCS (branch if carry set)
+        OpCode::new(0xb0,"BCS",2,2,AddressingMode::NoneAddressing),
+
+        // BEQ (branch if equal)
+        OpCode::new(0xf0,"BEQ",2,2,AddressingMode::NoneAddressing),
+
+        // BIT (bit test)
+        OpCode::new(0x24,"BIT",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0x2c,"BIT",3,4,AddressingMode::NoneAddressing),
+
+        // BMI (branch if minus)
+        OpCode::new(0x30,"BMI",2,2,AddressingMode::NoneAddressing),
+
+        // BNE (branch if not eq)
+        OpCode::new(0xd0,"BNE",2,2,AddressingMode::NoneAddressing),
+
+        // BPL (branch if positive)
+        OpCode::new(0x10,"BPL",2,2,AddressingMode::NoneAddressing),
+
+        // BVC (branch if overflow clear)
+        OpCode::new(0x50,"BVC",2,2,AddressingMode::NoneAddressing),
+
+        // BVS (branch if overflow set)
+        OpCode::new(0x70,"BVS",2,2,AddressingMode::NoneAddressing),
+
+        // CLC (clear carry flag)
+        OpCode::new(0x18,"CLC",1,2,AddressingMode::NoneAddressing),
+
+        // CLD (clear decimal mode)
+        OpCode::new(0xd8,"CLD",1,2,AddressingMode::NoneAddressing),
+
+        // CLI (clear interrupt disable)
+        OpCode::new(0x58,"CLI",1,2,AddressingMode::NoneAddressing),
+
+        // CLV (clear overflow flag)
+        OpCode::new(0xb8,"CLV",1,2,AddressingMode::NoneAddressing),
+
+        // CMP (compare)
+        OpCode::new(0xc9,"CMP",2,2,AddressingMode::Immediate),
+        OpCode::new(0xc5,"CMP",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0xd5,"CMP",2,4,AddressingMode::ZeroPage_X),
+        OpCode::new(0xcd,"CMP",3,4,AddressingMode::Absolute),
+        OpCode::new(0xdd,"CMP",3,4,AddressingMode::Absolute_X),
+        OpCode::new(0xd9,"CMP",3,4,AddressingMode::Absolute_Y),
+        OpCode::new(0xc1,"CMP",2,6,AddressingMode::Indirect_X),
+        OpCode::new(0xd1,"CMP",2,5,AddressingMode::Indirect_Y),
+
+        // CPX (compare X register)
+        OpCode::new(0xe0,"CPX",2,2,AddressingMode::Immediate),
+        OpCode::new(0xe4,"CPX",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0xec,"CPX",3,4,AddressingMode::Absolute),
+
+        // CPY (compare Y register)
+        OpCode::new(0xc0,"CPY",2,2,AddressingMode::Immediate),
+        OpCode::new(0xc4,"CPY",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0xcc,"CPY",3,4,AddressingMode::Absolute),
+
+        // DEC (decrement memory)
+        OpCode::new(0xc6,"DEC",2,5,AddressingMode::ZeroPage),
+        OpCode::new(0xd6,"DEC",2,6,AddressingMode::ZeroPage_X),
+        OpCode::new(0xce,"DEC",3,6,AddressingMode::Absolute),
+        OpCode::new(0xde,"DEC",3,7,AddressingMode::Absolute_X),
+
+        // DEX (decrement X register)
+        OpCode::new(0xca,"DEX",1,2,AddressingMode::NoneAddressing),
+
+        // DEY (decrement Y register)
+        OpCode::new(0x88,"DEY",1,2,AddressingMode::NoneAddressing),
+
+        // EOR (exclusive OR)
+        OpCode::new(0x49,"EOR",2,2,AddressingMode::Immediate),
+        OpCode::new(0x45,"EOR",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0x55,"EOR",2,4,AddressingMode::ZeroPage_X),
+        OpCode::new(0x4d,"EOR",3,4,AddressingMode::Absolute),
+        OpCode::new(0x5d,"EOR",3,4,AddressingMode::Absolute_X),
+        OpCode::new(0x59,"EOR",3,4,AddressingMode::Absolute_Y),
+        OpCode::new(0x41,"EOR",2,6,AddressingMode::Indirect_X),
+        OpCode::new(0x51,"EOR",2,5,AddressingMode::Indirect_Y),
+
+        // INC (increment memory)
+        OpCode::new(0xe6,"INC",2,5,AddressingMode::ZeroPage),
+        OpCode::new(0xf6,"INC",2,6,AddressingMode::ZeroPage_X),
+        OpCode::new(0xee,"INC",3,6,AddressingMode::Absolute),
+        OpCode::new(0xfe,"INC",3,7,AddressingMode::Absolute_X),
+
+        // INX (increment X)
+        OpCode::new(0xe8,"INX",1,2,AddressingMode::NoneAddressing),
+
+        // INY (increment Y)
+        OpCode::new(0xc8,"INY",1,2,AddressingMode::NoneAddressing),
+
+        // JMP (jump)
+        OpCode::new(0x4c,"JMP",3,3,AddressingMode::Absolute),
+        OpCode::new(0x6c,"JMP",3,5,AddressingMode::NoneAddressing),
+
+        // JSR (jump to subroutine)
+        OpCode::new(0x20,"JSR",3,6,AddressingMode::Absolute),
+
+        // LDX (load X reg)
+        OpCode::new(0xa2,"LDX",2,2,AddressingMode::Immediate),
+        OpCode::new(0xa6,"LDX",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0xb6,"LDX",2,4,AddressingMode::ZeroPage_Y),
+        OpCode::new(0xae,"LDX",3,4,AddressingMode::Absolute),
+        OpCode::new(0xbe,"LDX",3,4,AddressingMode::Absolute_Y),
+
+        // LDY (load Y reg)
+        OpCode::new(0xa0,"LDY",2,2,AddressingMode::Immediate),
+        OpCode::new(0xa4,"LDY",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0xb4,"LDY",2,4,AddressingMode::ZeroPage_X),
+        OpCode::new(0xac,"LDY",3,4,AddressingMode::Absolute),
+        OpCode::new(0xbc,"LDY",3,4,AddressingMode::Absolute_X),
+
+        // LSR (logical shift right)
+        OpCode::new(0x4a,"LSR",1,2,AddressingMode::NoneAddressing),
+        OpCode::new(0x46,"LSR",2,5,AddressingMode::ZeroPage),
+        OpCode::new(0x56,"LSR",2,6,AddressingMode::ZeroPage_X),
+        OpCode::new(0x4e,"LSR",3,6,AddressingMode::Absolute),
+        OpCode::new(0x5e,"LSR",3,7,AddressingMode::Absolute_X),
+
+        // NOP (no op)
+        OpCode::new(0xea,"NOP",1,2,AddressingMode::NoneAddressing),
+
+        // ORA (logical inclusive OR)
+        OpCode::new(0x09,"ORA",2,2,AddressingMode::Immediate),
+        OpCode::new(0x05,"ORA",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0x15,"ORA",2,4,AddressingMode::ZeroPage_X),
+        OpCode::new(0x0d,"ORA",3,4,AddressingMode::Absolute),
+        OpCode::new(0x1d,"ORA",3,4,AddressingMode::Absolute_X),
+        OpCode::new(0x19,"ORA",3,4,AddressingMode::Absolute_Y),
+        OpCode::new(0x01,"ORA",2,6,AddressingMode::Indirect_X),
+        OpCode::new(0x11,"ORA",2,5,AddressingMode::Indirect_Y),
+
+        // PHA (push accumulator)
+        OpCode::new(0x48,"PHA",1,3,AddressingMode::NoneAddressing),
+
+        // PHP (push processor status)
+        OpCode::new(0x08,"PHP",1,3,AddressingMode::NoneAddressing),
+
+        // PLA (pull accumulator)
+        OpCode::new(0x68,"PLA",1,4,AddressingMode::NoneAddressing),
+
+        // PLP (pull processor status)
+        OpCode::new(0x28,"PLP",1,4,AddressingMode::NoneAddressing),
+
+        // ROL (rotate left)
+        OpCode::new(0x2a,"ROL",1,2,AddressingMode::NoneAddressing),
+        OpCode::new(0x26,"ROL",2,5,AddressingMode::ZeroPage),
+        OpCode::new(0x36,"ROL",2,6,AddressingMode::ZeroPage_X),
+        OpCode::new(0x2e,"ROL",3,6,AddressingMode::Absolute),
+        OpCode::new(0x3e,"ROL",3,7,AddressingMode::Absolute_X),
+
+        // ROR (rotate right)
+        OpCode::new(0x6a,"ROR",1,2,AddressingMode::NoneAddressing),
+        OpCode::new(0x66,"ROR",2,5,AddressingMode::ZeroPage),
+        OpCode::new(0x76,"ROR",2,6,AddressingMode::ZeroPage_X),
+        OpCode::new(0x6e,"ROR",3,6,AddressingMode::Absolute),
+        OpCode::new(0x72,"ROR",3,7,AddressingMode::Absolute_X),
+
+        // RTI (return from interrupt)
+        OpCode::new(0x40,"RTI",1,6,AddressingMode::NoneAddressing),
+
+        // RTS (return from subroutine)
+        OpCode::new(0x60,"RTS",1,6,AddressingMode::NoneAddressing),
+
+        // SBC (subtract with carry)
+        OpCode::new(0xe9,"SBC",2,2,AddressingMode::Immediate),
+        OpCode::new(0xe5,"SBC",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0xf5,"SBC",2,4,AddressingMode::ZeroPage_X),
+        OpCode::new(0xed,"SBC",3,4,AddressingMode::Absolute),
+        OpCode::new(0xfd,"SBC",3,4,AddressingMode::Absolute_X),
+        OpCode::new(0xf9,"SBC",3,4,AddressingMode::Absolute_Y),
+        OpCode::new(0xe1,"SBC",2,6,AddressingMode::Indirect_X),
+        OpCode::new(0xf1,"SBC",2,5,AddressingMode::Indirect_Y),
+
+        // SEC (set carry flag)
+        OpCode::new(0x38,"SEC",1,2,AddressingMode::NoneAddressing),
+
+        // SED (set decimal flag)
+        OpCode::new(0xf8,"SED",1,2,AddressingMode::NoneAddressing),
+
+        // SEI (set interrupt disable)
+        OpCode::new(0x78,"SEI",1,2,AddressingMode::NoneAddressing),
+
+        // STX (store reg X)
+        OpCode::new(0x86,"STX",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0x96,"STX",2,4,AddressingMode::ZeroPage_Y),
+        OpCode::new(0x8e,"STX",3,4,AddressingMode::Absolute),
+
+        // STY (store reg Y)
+        OpCode::new(0x84,"STY",2,3,AddressingMode::ZeroPage),
+        OpCode::new(0x94,"STY",2,4,AddressingMode::ZeroPage_X),
+        OpCode::new(0x8c,"STY",3,4,AddressingMode::Absolute),
+
+        // TAY (transfer A to Y)
+        OpCode::new(0xa8,"TAY",1,2,AddressingMode::NoneAddressing),
+
+        // TSX (transfer SP to X)
+        OpCode::new(0xba,"TSX",1,2,AddressingMode::NoneAddressing),
+
+        // TXA (transfer X to A)
+        OpCode::new(0x8a,"TXA",1,2,AddressingMode::NoneAddressing),
+
+        // TXS (transfer X to SP)
+        OpCode::new(0x9a,"TXS",1,2,AddressingMode::NoneAddressing),
+
+        // TYA (transfer Y to A)
+        OpCode::new(0x98,"TYA",1,2,AddressingMode::NoneAddressing)
+
     ];
 
     pub static ref OPCODES_MAP: HashMap<u8,&'static OpCode> = {
